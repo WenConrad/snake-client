5d6d3aae0adeb995ded9d609033011dfbdf91614
WenConrad <conradwen@gmail.com>
2021-11-03 11:42:53 -0700

    add emotes

--- input.js
+++ input.js
@@ -4,15 +4,38 @@ const handleUserInput = function (key) {
   }
   if (key === 'w') {
     connection.write("Move: up");
-  }
-  if (key === 'a') {
+  } else if (key === 'a') {
     connection.write("Move: left");
-  }
-  if (key === 's') {
+  } else if (key === 's') {
     connection.write("Move: down");
-  }
-  if (key === 'd') {
+  } else if (key === 'd') {
     connection.write("Move: right");
+  } else if (key === 'i') {
+    for (i = 0; i < 5; i++) {
+      setTimeout(() => {connection.write("Move: up")}, 50*i);
+    }
+  } else if (key === 'j') {
+    for (i = 0; i < 5; i++) {
+      setTimeout(() => {connection.write("Move: left")}, 50*i);
+    }
+  } else if (key === 'k') {
+    for (i = 0; i < 5; i++) {
+      setTimeout(() => {connection.write("Move: down")}, 50*i);
+    }
+  } else if (key === 'l') {
+    for (i = 0; i < 5; i++) {
+      setTimeout(() => {connection.write("Move: right")}, 50*i);
+    }
+  } else if (key === '1') {
+    connection.write("Say: Well Met!");
+  } else if (key === '2') {
+    connection.write("Say: I need healing!");
+  } else if (key === '3') {
+    connection.write("Say: ?");
+  } else if (key === '4') {
+    connection.write("Say: PKPKPKPK");
+  } else if (key === '5') {
+    connection.write("Say: 1v1 me bro");
   }
 };
 

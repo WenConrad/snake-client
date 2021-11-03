6a1891c72af19d74bf0938ab6a6db86903e00f9e
WenConrad <conradwen@gmail.com>
2021-11-03 10:07:29 -0700

    can connect to server

---
+++ play.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541',
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect',function(message){
+    console.log(message);
+  });
+
+  conn.on('data', function(message){
+    console.log(message);
+  });
+
+  // process.stdin.on('data', function(message){
+  //   client.write(message);
+  // });
+
+  conn.on('end', function(message){
+    console.log(message);
+  });
+    
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
